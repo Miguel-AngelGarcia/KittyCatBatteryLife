1e225f2bcf4955e029110afb021b7b2b1f8ee42d
Miguel-AngelGarcia <mgar574@wgu.edu>
2022-10-25 21:41:27 -0400

    Initial Commit

---
+++ KittyCatBatteryLife/KittyCatBatteryLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyCatBatteryLifeApp.swift
+//  KittyCatBatteryLife
+//
+//  Created by Miguel Garcia on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct KittyCatBatteryLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
